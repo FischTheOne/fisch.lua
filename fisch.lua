7e2d69d693adfe1b05ac2bad12175a5d14c05777
FischTheOne <elias-b.fischer@web.de>
2024-12-14 21:18:58 +0100

    Create fisch.lua

---
+++ fisch.lua
@@ -0,0 +1,10 @@
+wait(3)
+
+local function highlight(player)
+    local highlight = Instance.new("Highlight")
+    highlight.Parent = player.Character
+end
+
+for _, player in pairs(game.Players:GetPlayers()) do
+    highlight(player)
+end
