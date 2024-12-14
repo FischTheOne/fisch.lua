89124b0cb4bfcc99c046c093d9be63ee2fe69e28
FischTheOne <elias-b.fischer@web.de>
2024-12-14 22:31:22 +0100

    Update ped.lua

--- ped.lua
+++ ped.lua
@@ -1,17 +1,9 @@
-local yourUsername = "elias123_robo"
-local increasedSpeed = 50
-local normalSpeed = 16
-
-local function adjustSpeed(player)
-    player.CharacterAdded:Connect(function(character)
-        local humanoid = character:WaitForChild("Humanoid")
-        humanoid.WalkSpeed = (player.Name == yourUsername) and increasedSpeed or normalSpeed
-    end)
-end
-
 local Players = game:GetService("Players")
-Players.PlayerAdded:Connect(adjustSpeed)
 
-for , player in ipairs(Players:GetPlayers()) do
-    adjustSpeed(player)
+local function speed()
+    game.StarterPlayer.CharacterWalkSpeed = 100
 end
+
+wait(2)
+
+speed()
